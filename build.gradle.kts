4cc55355bec728657fb9bf2f5aabf0dd59a5d496
arm32x <github.account@arm32.ax>
2025-03-21 23:02:24 -0400

    Increase Java language version to Java 8
    It's 2025, and Java 7 is thoroughly dead at this point. Java 8 is still
    hanging on for legacy support, which I guess this is...
    
    In the past, opsu! has crashed when I tried to run it on Java versions
    newer than 8, so I'm stuck with that for now. However, I am interested
    in the new Foreign Function and Memory API, so I probably won't stay on
    Java 8 forever.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ application {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 sourceSets["main"].java.setSrcDirs(listOf("src"))
