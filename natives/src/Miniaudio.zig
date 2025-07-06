1bc9822dc0e948469747d3ac71a9a506dcaa1a24
arm32x <github.account@arm32.ax>
2025-07-06 13:42:25 -0400

    Set miniaudio options for low latency
    noFixedSizeCallback seems to have the biggest impact here.
    ma_performance_profile_low_latency is the default, but I set it
    explicitly here just to be clear.

--- natives/src/Miniaudio.zig
+++ natives/src/Miniaudio.zig
@@ -6,6 +6,8 @@ const utils = @import("utils.zig");
 
 const Self = @This();
 
+context: ma.ma_context,
+device: ma.ma_device,
 engine: ma.ma_engine,
 
 pub fn jniInit(cEnv: *jni.cEnv, class: jni.jclass) callconv(.C) jni.jlong {
@@ -15,15 +17,49 @@ pub fn jniInit(cEnv: *jni.cEnv, class: jni.jclass) callconv(.C) jni.jlong {
 
 fn init(env: jni.JNIEnv, _: jni.jclass) error{Exception}!jni.jlong {
     const allocator = std.heap.c_allocator;
+    var result: ma.ma_result = ma.MA_SUCCESS;
 
     const self = allocator.create(Self) catch return utils.throwOutOfMemoryError(env);
 
-    const result = ma.ma_engine_init(null, &self.engine);
+    // TODO: Allow user to choose which backend they want to use
+    result = ma.ma_context_init(null, 0, null, &self.context);
+    if (result != ma.MA_SUCCESS) {
+        return utils.throwMiniaudioException(env, result, "Failed to create miniaudio context");
+    }
+
+    var device_config = ma.ma_device_config_init(ma.ma_device_type_playback);
+    device_config.playback.format = ma.ma_format_f32;
+    device_config.playback.channels = 2;
+    device_config.dataCallback = dataCallback;
+    device_config.pUserData = self;
+    device_config.performanceProfile = ma.ma_performance_profile_low_latency;
+    device_config.periodSizeInMilliseconds = 5;
+    device_config.noFixedSizedCallback = 1;
+    result = ma.ma_device_init(&self.context, &device_config, &self.device);
+    if (result != ma.MA_SUCCESS) {
+        return utils.throwMiniaudioException(env, result, "Failed to create miniaudio device");
+    }
+
+    var engine_config = ma.ma_engine_config_init();
+    // engine_config.pContext = &self.context;
+    engine_config.pDevice = &self.device;
+    result = ma.ma_engine_init(&engine_config, &self.engine);
     if (result != ma.MA_SUCCESS) {
         return utils.throwMiniaudioException(env, result, "Failed to create miniaudio engine");
     }
 
     return utils.handleFromPtr(self);
+}
+
+fn dataCallback(
+    device: ?*ma.ma_device,
+    output: ?*anyopaque,
+    input: ?*const anyopaque,
+    frame_count: ma.ma_uint32,
+) callconv(.C) void {
+    _ = input;
+    const self: *Self = @ptrCast(@alignCast(device.?.pUserData.?));
+    _ = ma.ma_engine_read_pcm_frames(&self.engine, output, frame_count, null);
 }
 
 pub fn jniDestroy(cEnv: *jni.cEnv, class: jni.jclass, handle: jni.jlong) callconv(.C) void {
@@ -36,6 +72,8 @@ fn destroy(_: jni.JNIEnv, _: jni.jclass, handle: jni.jlong) error{Exception}!voi
     const self = utils.ptrFromHandle(Self, handle);
 
     ma.ma_engine_uninit(&self.engine);
+    ma.ma_device_uninit(&self.device);
+    _ = ma.ma_context_uninit(&self.context);
 
     allocator.destroy(self);
 }
